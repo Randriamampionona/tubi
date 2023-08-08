0e918d15a5a0c0ad4b6517df561778ad98c0174a
Randriamampionona <tojorandriaII474@gmail.com>
2023-08-08 14:40:28 +0300

    first stable version

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Link from "next/link"
 
 const Home = () => {
   return (
-    <div className="flex items-end justify-center h-full">
-      <h1 className="text-center">Welcome to Tubi, <u>go to <Link href={"/chats"}>Chats</Link></u></h1>
+    <div className="flex items-end justify-center">
+      <h1 className="text-center mt-48">Welcome to Tubi v1.0.0, <u>go to <Link href={"/chats"}>Chats</Link></u></h1>
     </div>
   )
 }
